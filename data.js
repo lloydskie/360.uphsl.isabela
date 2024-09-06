0cc85f1a7ed83a48b8f53503f13112fc125128a9
John Lloyd Quijano <84909074+lloydskie@users.noreply.github.com>
2024-09-06 06:48:24 -0400

    update

---
+++ data.js
@@ -0,0 +1,907 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-parking2",
+      "name": "PARKING2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6533631125573649,
+          "pitch": 0.1245253684115486,
+          "rotation": 0,
+          "target": "1-parking"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-parking",
+      "name": "PARKING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0012434183138942956,
+          "pitch": 0.13552072142132765,
+          "rotation": 0,
+          "target": "0-parking2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-clinic_",
+      "name": "Clinic_",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0430995433818824,
+          "pitch": 0.13583736579174044,
+          "rotation": 0,
+          "target": "3-hotel-lobby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-hotel-lobby",
+      "name": "HOTEL LOBBY",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.9260560616914191,
+        "pitch": 0.09827501790789483,
+        "fov": 1.380609968286806
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.604092561496314,
+          "pitch": 0.16916377655725512,
+          "rotation": 0,
+          "target": "5-2ay2tindalo"
+        },
+        {
+          "yaw": 0.22166269045114007,
+          "pitch": 0.10811334501777026,
+          "rotation": 0,
+          "target": "3-hotel-lobby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-research-office",
+      "name": "RESEARCH OFFICE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9185550403306593,
+          "pitch": 0.07785875320041136,
+          "rotation": 0,
+          "target": "3-hotel-lobby"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-2ay2tindalo",
+      "name": "2AY2TINDALO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8182882982480963,
+          "pitch": 0.25348367072366074,
+          "rotation": 0,
+          "target": "5-2ay2tindalo"
+        },
+        {
+          "yaw": 1.5601984285763164,
+          "pitch": 0.2860122636591882,
+          "rotation": 0,
+          "target": "8-brickground"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-tindalo-hall",
+      "name": "TINDALO HALL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.615030545655733,
+          "pitch": 0.2894283985001671,
+          "rotation": 0,
+          "target": "7-tindalo"
+        },
+        {
+          "yaw": -1.0979870303605281,
+          "pitch": 0.2652416399036248,
+          "rotation": 0,
+          "target": "9-tesda"
+        },
+        {
+          "yaw": 3.1170966641732694,
+          "pitch": 0.21859945475775966,
+          "rotation": 0,
+          "target": "5-2ay2tindalo"
+        },
+        {
+          "yaw": -2.617203921332072,
+          "pitch": 0.1788959291701815,
+          "rotation": 0,
+          "target": "8-brickground"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-tindalo",
+      "name": "TINDALO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.244832988595178,
+          "pitch": 0.10313173375359952,
+          "rotation": 0,
+          "target": "6-tindalo-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-brickground",
+      "name": "BRICKGROUND",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.641500480659606,
+          "pitch": 0.028292305846679966,
+          "rotation": 0,
+          "target": "5-2ay2tindalo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-tesda",
+      "name": "TESDA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2349523900278445,
+          "pitch": -0.11393612593716895,
+          "rotation": 0,
+          "target": "6-tindalo-hall"
+        },
+        {
+          "yaw": 0.998399338214556,
+          "pitch": 0.18012651850725625,
+          "rotation": 0,
+          "target": "10-preschool-basem"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-preschool-basem",
+      "name": "PRESCHOOL BASEM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.24714646019080533,
+        "pitch": 0.1527453823719469,
+        "fov": 1.380609968286806
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8241481466158334,
+          "pitch": -0.024463161094203656,
+          "rotation": 0,
+          "target": "9-tesda"
+        },
+        {
+          "yaw": 0.14264586353122333,
+          "pitch": 0.11695164816821091,
+          "rotation": 0,
+          "target": "11-preschool"
+        },
+        {
+          "yaw": -2.2509282104378077,
+          "pitch": 0.1528211986798098,
+          "rotation": 0,
+          "target": "12-preschool-rm"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-preschool",
+      "name": "PRESCHOOL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.5762986230134519,
+        "pitch": 0.15511767183322434,
+        "fov": 1.380609968286806
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8392071669282126,
+          "pitch": 0.14042009311979875,
+          "rotation": 0,
+          "target": "10-preschool-basem"
+        },
+        {
+          "yaw": -2.994787583809188,
+          "pitch": 0.10925577627019578,
+          "rotation": 0,
+          "target": "13-gr1_b7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-preschool-rm",
+      "name": "PRESCHOOL RM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 3.0250008861658895,
+        "pitch": 0.24826489076675884,
+        "fov": 1.380609968286806
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2462351928997952,
+          "pitch": -0.06949244986258662,
+          "rotation": 0,
+          "target": "10-preschool-basem"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-gr1_b7",
+      "name": "GR1_B7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6256258046887657,
+          "pitch": 0.08892103434018139,
+          "rotation": 0,
+          "target": "11-preschool"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-b1",
+      "name": "B1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-b2",
+      "name": "B2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-consultation-rm",
+      "name": "CONSULTATION RM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-nursing3",
+      "name": "NURSING3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "18-nursing-rm2",
+      "name": "NURSING RM2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "19-nursing-rm1",
+      "name": "NURSING RM1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "20-20240604_122209_063",
+      "name": "20240604_122209_063",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "21-20240604_122221_798",
+      "name": "20240604_122221_798",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "22-20240604_122228_244",
+      "name": "20240604_122228_244",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "23-20240604_122247_023",
+      "name": "20240604_122247_023",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "UPHSL-IC Virtual Campus Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
